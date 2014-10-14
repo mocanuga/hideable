7b46b36d29430de0a50640db870147f1e01b5c8c
Adi <mocanugeorge@gmail.com>
2014-10-14 18:54:08 +0300

    Update hideable.js

--- hideable.js
+++ hideable.js
@@ -13,11 +13,13 @@
             $(d).off('mouseup.hideable').on('mouseup.hideable', function (e) {
                 if (!$self.is(e.target) && $self.find(e.target).length === 0) {
                     console.log(typeof opts.beforeHide);
-                    if(typeof opts.beforeHide === 'function')
+                    if(typeof opts.beforeHide === 'function') {
                         opts.beforeHide.apply(this, [$self]);
+                    }
                     $self.hide();
-                    if(typeof opts.afterHide === 'function')
+                    if(typeof opts.afterHide === 'function') {
                         opts.afterHide.apply(this, [$self]);
+                    }
                 }
             });
         });
